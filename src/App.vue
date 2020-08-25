13b4a71a47d0f95a6facbbbcddba114fc2e1fdd4
Kacper Kozanecki <kacper2424@interia.eu>
2020-08-25 18:58:17 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class='base' />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: aliceblue;
+  }
+
+  #app {
+    background-color: blue;
+  }
+
+  .base {
+    width: 70%;
+    height: 50px;
+
+    position: fixed;
+    top: 70%;
+    left: 50%;
+    transform: translateX(-50%);
+
+    background-color: brown;
+    border-radius: 10px;
+
+
+  }
 </style>
